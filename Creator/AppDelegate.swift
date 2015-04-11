4225fd97d634e442041083823b139264786bccb6
Litherum <litherum@gmail.com>
2015-04-10 19:31:28 -0700

    Initial Commit

---
+++ Creator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Creator
+//
+//  Created by Litherum on 4/10/15.
+//  Copyright (c) 2015 Litherum. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
